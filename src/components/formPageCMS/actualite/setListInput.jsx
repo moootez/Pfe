0dbfb61d8f92a16c71872e9953d210a991e79b8a
oumaima boukhris <oumaima.boukhris@inetum.com>
2021-10-19 10:49:08 +0100

    FeatActualite

--- src/components/formPageCMS/actualite/setListInput.jsx
+++ src/components/formPageCMS/actualite/setListInput.jsx
@@ -24,13 +24,42 @@ const setListInput = () => {
 
         {
             name: 'priorite',
+            required: true,
+            label: 'PRIORITE',
+            list: [
+                {
+                    label: '1',
+                    value: 1,
+                },
+                {
+                    label: '2',
+                    value: 2,
+                },
+                {
+                    label: '3',
+                    value: 3,
+                },
+                {
+                    label: '4',
+                    value: 4,
+                },
+                {
+                    label: '5',
+                    value: 5,
+                },
+                {
+                    label: '6',
+                    value: 6,
+                },
+            ],
+            isSelect: true,
+            selectAll: true,
             sm: 6,
             md: 3,
-            label: 'Priorite',
             placeholder: 'Priorite',
             lng: 'fr',
-            required: false,
         },
+
         /* {
             name: 'dateActualite',
             label: 'Date Publication',
