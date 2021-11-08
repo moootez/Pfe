a9ec88e9637806459751aebfdb7d665ed5b0b375
oumaima boukhris <oumaima.boukhris@inetum.com>
2021-11-08 15:22:45 +0100

    Feat TableCollapse

--- src/serveur/baseUrl.js
+++ src/serveur/baseUrl.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-mutable-exports
-const baseUrl = 'http://40.115.23.121:8080/api/'
-// const baseUrl = 'http://10.1.1.177/api/'
+// const baseUrl = 'http://40.115.23.121:8080/api/'
+const baseUrl = 'http://10.1.1.177/api/'
 // baseUrl preprod
 
 export default baseUrl
